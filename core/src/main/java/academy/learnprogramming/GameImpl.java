df1efdcf48960f50612e8bb2b722d57ced6f1425
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-10-04 09:36:18 +0200

    Spring

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -1,25 +1,30 @@
 package academy.learnprogramming;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
 
     // == constants ==
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
+ //   private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Getter(AccessLevel.NONE)
     private final NumberGenerator numberGenerator;
 
     private final int guessCount;
 
     private int number;
+    @Setter
     private int guess;
     private int smallest;
     private int biggest;
@@ -51,40 +56,40 @@ public class GameImpl implements Game {
     }
 
     // == public methods ==
-    @Override
-    public int getNumber() {
-        return number;
-    }
+//    @Override
+//    public int getNumber() {
+//        return number;
+//    }
+//
+//    @Override
+//    public int getGuess() {
+//        return guess;
+//    }
 
-    @Override
-    public int getGuess() {
-        return guess;
-    }
+//    @Override
+//    public void setGuess(int guess) {
+//        this.guess = guess;
+//    }
 
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() {
-        return guessCount;
-    }
+//    @Override
+//    public int getSmallest() {
+//        return smallest;
+//    }
+//
+//    @Override
+//    public int getBiggest() {
+//        return biggest;
+//    }
+//
+//    @Override
+//    public int getRemainingGuesses() {
+//        return remainingGuesses;
+//    }
+//
+//    @Override
+//    public int getGuessCount() {
+//        return guessCount;
+//    }
 
     @Override
     public void check() {
@@ -104,10 +109,10 @@ public class GameImpl implements Game {
         remainingGuesses--;
     }
 
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
-    }
+//    @Override
+//    public boolean isValidNumberRange() {
+//        return validNumberRange;
+//    }
 
     @Override
     public boolean isGameWon() {
